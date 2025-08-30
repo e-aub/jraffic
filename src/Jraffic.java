9254dc21a8704ea00615fed4baf24dfbca3cac12
anass-asbai <asanass208@gmail.com>
2025-08-30 16:05:06 +0100

    add vehicle

--- src/Jraffic.java
+++ src/Jraffic.java
@@ -2,7 +2,7 @@ import processing.core.PApplet;
 import java.util.*;
 
 public class Jraffic extends PApplet {
-    // List<Vehicle> vehicles = new ArrayList<Vehicle>();
+    List<Vehicle> vehicles = new ArrayList<Vehicle>();
     List<TrafficLight> trafficLights = new ArrayList<TrafficLight>();
 
     public static void main(String[] args) {
@@ -21,8 +21,43 @@ public class Jraffic extends PApplet {
 
     public void draw() {
         background(0);
+        if (vehicles.size() != 0) {
+            update();
+            for (Vehicle v : vehicles) {
+                switch (v.getDirection()) {
+                    case Right: {
+                        fill(255);
+                        break;
+                    }
+                    case Left: {
+                        fill(0, 255, 0);
+                        break;
+                    }
+                    default: {
+                        fill(0, 0, 255);
+                    }
+
+                }
+                v.checkTourne();
+                rect((float) v.getPosition().x, (float) v.getPosition().y, 50, 50);
+            }
+        }
         road();
 
+    }
+
+    public void update() {
+        for (Vehicle v : vehicles) {
+            if (v.getRoute() == Route.North) {
+                v.setPosition(new Vec2(v.getPosition().x, v.getPosition().y - v.getSpeed()));
+            } else if (v.getRoute() == Route.South) {
+                v.setPosition(new Vec2(v.getPosition().x, v.getPosition().y + v.getSpeed()));
+            } else if (v.getRoute() == Route.East) {
+                v.setPosition(new Vec2(v.getPosition().x - v.getSpeed(), v.getPosition().y));
+            } else if (v.getRoute() == Route.West) {
+                v.setPosition(new Vec2(v.getPosition().x + v.getSpeed(), v.getPosition().y));
+            }
+        }
     }
 
     public void rectM() {
@@ -43,14 +78,85 @@ public class Jraffic extends PApplet {
         line((width / 2) + 50, 0, (width / 2) + 50, height);
         line((width / 2) - 50, 0, (width / 2) - 50, height);
         fill(255, 0, 0);
+
         for (TrafficLight tl : trafficLights) {
             if (tl.getState() == LightState.RED) {
                 fill(255, 0, 0);
             } else {
                 fill(0, 255, 0);
             }
-           
             rect((float) tl.getPosition().x, (float) tl.getPosition().y, 50, 50);
         }
+    }
+
+    public void keyPressed() {
+
+        Route route = null;
+        if (key == CODED || key == ' ') {
+            if (key == ' ') {
+                Random rand = new Random();
+                int r = rand.nextInt(4);
+                if (r == 0)
+                    route = Route.North;
+                else if (r == 1)
+                    route = Route.South;
+                else if (r == 2)
+                    route = Route.East;
+                else
+                    route = Route.West;
+            }
+
+            if (key == CODED) {
+                switch (keyCode) {
+                    case UP:
+                        route = Route.North;
+                        break;
+                    case DOWN:
+                        route = Route.South;
+                        break;
+                    case LEFT:
+                        route = Route.East;
+                        break;
+                    case RIGHT:
+                        route = Route.West;
+                        break;
+                }
+            }
+
+            if (route != null) {
+                Vec2 newPos = null;
+                switch (route) {
+                    case North:
+                        newPos = new Vec2(width / 2, height - 50);
+                        break;
+                    case South:
+                        newPos = new Vec2((width / 2) - 50, 0);
+                        break;
+                    case East:
+                        newPos = new Vec2(width - 50, height / 2 - 50);
+                        break;
+                    case West:
+                        newPos = new Vec2(0, height / 2);
+                        break;
+                }
+
+                boolean canAdd = true;
+                float minDistance = 100;
+                for (Vehicle v : vehicles) {
+                    float d = dist((float) newPos.x, (float) newPos.y, (float) v.getPosition().x,
+                            (float) v.getPosition().y);
+
+                    if (d < minDistance&&route==v.getRoute()) {
+                        canAdd = false;
+                        break;
+                    }
+                }
+
+                if (canAdd) {
+                    vehicles.add(new Vehicle(newPos, route));
+                }
+            }
+        }
+
     }
 }
--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,3 +1,132 @@
+import java.util.Random;
+
 public class Vehicle {
-    
+    private Vec2 position;
+    private Route route;
+    private final double speed = 1.0;
+    private boolean isMoving = true;
+    private Direction direction;
+    private boolean turned = true;
+
+    public Vehicle(Vec2 position, Route route) {
+        this.position = position;
+        this.route = route;
+        Random rand = new Random();
+        int index = rand.nextInt(3);
+
+        switch (index) {
+            case 0:
+                direction = Direction.Left;
+                break;
+            case 1:
+                direction = Direction.Right;
+                break;
+            default:
+                direction = Direction.Straight;
+        }
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public Vec2 getPosition() {
+        return position;
+    }
+
+    public void checkTourne() {
+        if (!turned) {
+            return;
+        }
+        Vec2 pos = getPosition();
+        double x = pos.x;
+        double y = pos.y;
+
+        switch (direction) {
+            case Right: {
+                switch (route) {
+                    case North:
+                        if (y == 500) {
+                            route = Route.West;
+                            turned = false;
+                        }
+                        break;
+                    case South:
+                        if (y == 450) {
+                            route = Route.East;
+                            turned = false;
+                        }
+                        break;
+                    case East:
+                        if (x == 500) {
+                            route = Route.North;
+                            turned = false;
+                        }
+                        break;
+                    default:
+                        if (x == 450) {
+                            route = Route.South;
+                            turned = false;
+                        }
+                        break;
+                }
+                break;
+            }
+            case Left: {
+                switch (route) {
+                    case North:
+                        if (y == 450) {
+                            route = Route.East;
+                            turned = false;
+                        }
+                        break;
+                    case South:
+                        if (y == 500) {
+                            route = Route.West;
+                            turned = false;
+                        }
+                        break;
+                    case East:
+                        if (x == 450) {
+                            route = Route.South;
+                            turned = false;
+                        }
+                        break;
+                    default:
+                        if (x == 500) {
+                            route = Route.North;
+                            turned = false;
+                        }
+                        break;
+                }
+                break;
+            }
+
+        }
+
+    }
+
+    public void setPosition(Vec2 position) {
+        this.position = position;
+    }
+
+    public Route getRoute() {
+        return route;
+    }
+
+    public void setRoute(Route route) {
+        this.route = route;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public boolean isMoving() {
+        return isMoving;
+    }
+
+    public void setMoving(boolean isMoving) {
+        this.isMoving = isMoving;
+    }
 }
